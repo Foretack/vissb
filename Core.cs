29efe234049096f3fe19c22f3acb410b8bfb9e8a
2vitreous <observer.in.a.box@gmail.com>
2022-02-12 11:31:15 +0100

    Add project files.

---
+++ Core.cs
@@ -0,0 +1,54 @@
+﻿using TwitchLib.Client;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+namespace Core
+{
+    public class Core
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new();
+            Console.ReadLine();
+        }
+
+    }
+
+    public class Bot
+    {
+        public static readonly string Username = "TWITCH_USERNAME";
+        public static readonly string Token = "ACCESS_TOKEN_GOES_HERE";
+
+        TwitchClient client;
+
+        public Bot()
+        {
+            ConnectionCredentials credentials = new ConnectionCredentials(Username, Token);
+            var clientOptions = new ClientOptions
+            {
+                MessagesAllowedInPeriod = 750,
+                ThrottlingPeriod = TimeSpan.FromSeconds(30)
+            };
+            WebSocketClient customClient = new WebSocketClient(clientOptions);
+            client = new TwitchClient(customClient);
+            client.Initialize(credentials, "minusinsanity");
+
+            client.OnJoinedChannel += (s, e) =>
+            {
+                Console.WriteLine($"Connected to {e.Channel}");
+            };
+            client.OnMessageReceived += (s, e) =>
+            {
+                //
+            };
+            client.OnConnected +=  (s, e) =>
+            {
+                Console.WriteLine("connected");
+                PubSub pubSub = new();
+            };
+
+            client.Connect();
+        }
+    }
+}
---
+++ PubSub.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using TwitchLib.PubSub;
+using TwitchLib.PubSub.Events;
+
+namespace Core
+{
+    public class PubSub
+    {
+        private TwitchPubSub? client;
+
+        public PubSub()
+        {
+            Run();
+        }
+
+        private void Run()
+        {
+            client = new TwitchPubSub();
+
+            client.OnPubSubServiceConnected += (s, e) =>
+            {
+                client.SendTopics();
+            };
+            client.OnListenResponse += (s, e) =>
+            {
+                if (!e.Successful) throw new Exception($"Failed to listen! Response: {e.Response}");
+                else Console.WriteLine($"listening to {e.Topic}");
+            };
+            client.OnStreamUp += (s, e) => 
+            {
+                Console.WriteLine($"Stream just went up! Play delay: {e.PlayDelay}, server time: {e.ServerTime}");
+            };
+            client.OnStreamDown += (s, e) =>
+            {
+                Console.WriteLine($"Stream just went down! Server time: {e.ServerTime}");
+            };
+            client.OnFollow += (s, e) =>
+            {
+                Console.WriteLine($"{e.Username} just followed!");
+            };
+            client.OnViewCount += (s, e) =>
+            {
+                Console.WriteLine($"{e.Viewers} {e.ServerTime}");
+            };
+
+            client.ListenToVideoPlayback("17497365");
+            client.ListenToFollows("17497365");
+
+            client.Connect();
+        }
+    }
+}
