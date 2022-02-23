4381a89f82a585d529d7609a1096d74a3fa0b5b4
2vitreous <observer.in.a.box@gmail.com>
2022-02-23 20:48:48 +0100

    sorry marv i hard to report you

--- AskCommand.cs
+++ AskCommand.cs
@@ -31,7 +31,7 @@ namespace Core
 
             RequestBody body = new()
             {
-                prompt = $"{Username}: {Input}\nMarv:",
+                prompt = $"{Username}: {Input}",
                 max_tokens = 90,
                 temperature = 0.5f,
                 top_p = 0.3f,
